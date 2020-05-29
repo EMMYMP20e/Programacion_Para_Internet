8f5e5874d7c9b88439befbac7fea60bdd8c7ec6d
Unknown <emmy.mp.98e@gmail.com>
2020-05-28 22:55:46 -0500

    Actualiza Stock

--- pFinal-FrontEnd/php/enviarPedido.php
+++ pFinal-FrontEnd/php/enviarPedido.php
@@ -7,6 +7,24 @@ require "conecta.php";
 $con = conecta();
 $id_pedido = $_REQUEST['id_pedido'];
 
+$sql = "SELECT * FROM 
+    ( SELECT * FROM pedidos_productos WHERE id_pedido = $id_pedido)
+    AS this_pedido
+    INNER JOIN productos ON this_pedido.id_producto = productos.id";
+
+$res = mysql_query($sql, $con);
+$num = mysql_num_rows($res);
+
+for ($i = 0; $i < $num; $i++) {
+    $id_producto = mysql_result($res, $i, "id_producto");
+    $cantidad = mysql_result($res, $i, "cantidad");
+    $stock = mysql_result($res, $i, "stock");
+    $stock-=$cantidad;
+
+    $sql = "UPDATE productos SET stock = $stock WHERE id=$id_producto";
+    mysql_query($sql, $con);
+}
+
 $sql = "UPDATE pedidos SET status = 1 WHERE id=$id_pedido";
 
 $res = mysql_query($sql, $con);
